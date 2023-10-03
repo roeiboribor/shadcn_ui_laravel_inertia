86499f4b71523a7ce0bed027d3958b3f8bca880d
Roei Boribor <roeiboribor@gmail.com>
2023-10-03 14:50:56 +0800

    initial changes on my preference

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $gender = fake()->randomElement(['male', 'female']);
+        $firstName = fake()->firstName($gender);
+        $lastName = fake()->lastName($gender);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $firstName . ' ' . $lastName,
+            'email' => strtolower(trim($firstName . '.' . $lastName)) . '@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
